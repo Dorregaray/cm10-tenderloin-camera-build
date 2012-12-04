785e60c9c9d9269399ce341679ddb00db4246605
Tomasz Rostanski <rostan@podkowa.net>
2012-12-04 16:23:18 +0100

    Update patches

--- apply-camera_patches.sh
+++ apply-camera_patches.sh
@@ -9,13 +9,8 @@ cd $TOPDIR/device/hp/tenderloin
 
 #camera hal
 echo "26085 BEGIN"
-git pull http://review.cyanogenmod.org/CyanogenMod/android_device_hp_tenderloin refs/changes/85/26085/8
+git pull http://review.cyanogenmod.org/CyanogenMod/android_device_hp_tenderloin refs/changes/85/26085/9
 echo "26085 END"
-
-#video encoder
-echo "26086 BEGIN"
-git pull http://review.cyanogenmod.org/CyanogenMod/android_device_hp_tenderloin refs/changes/86/26086/1
-echo "26086 END"
 
 cd $TOPDIR
 
